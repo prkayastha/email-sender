978d73ddc3ddd5d3ebc27135a25185d173f3a04f
Prajesh Raj Kayastha <94prajesh@gmail.com>
2020-03-15 12:37:15 +1030

    add route for adding user

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var router = express.Router();
 
+const userOperation = require('../controller/user');
+const User = require('../prototypes/users/users');
+
 /* GET users listing. */
-router.get('/', function(req, res) {
-  res.send('respond with a resource');
+router.get('/add', function (req, res) {
+  const user = new User();
+  user.setData(req.body);
+  userOperation.add(user).then(userResponse => {
+    res.send(userResponse);
+  });
 });
 
 module.exports = router;
