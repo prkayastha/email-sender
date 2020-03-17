bf046bae4e77e735c660f8bba8bc656025ecf2c2
Prajesh Raj Kayastha <94prajesh@gmail.com>
2020-03-17 19:14:17 +1030

    add method compareHash

--- controller/user/hashUtils.js
+++ controller/user/hashUtils.js
@@ -13,6 +13,23 @@ const generateHash = function (createdAt, email) {
     return crypto.createHash('sha256').update(stringToHash).digest('hex')
 }
 
-module.exports = {
-    generateHash
+/**
+ * function to compare hash for activating the user
+ * @param {string} hashString hash string from the URL
+ * @param {Object} user user object for the hash to be compared
+ * @returns {boolean} returns true if the passed hashString matches with the user else returns false
+ */
+const compareHash = function (hashString, user) {
+    const hashToCompare = generateHash(user.createdAt, user.email);
+    
+    if (hashString === hashToCompare) {
+        return true;
+    }
+
+    return false;
 }
+
+module.exports = {
+    generateHash,
+    compareHash
+};
