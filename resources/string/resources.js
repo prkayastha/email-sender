3a964188f1137763a712b4c1ece2c0977d8a1fcd
Prajesh Raj Kayastha <94prajesh@gmail.com>
2020-03-22 14:38:54 +1030

    add string resources for deleting a user

--- resources/string/resources.js
+++ resources/string/resources.js
@@ -5,14 +5,17 @@ const messages = {
         <p> <a title="activate_link" href="{0}" target="_blank">{0}</p>\
         <p>Thank You</p>\
         <p>Yours Sincere,<br/>The Team</p>',
-        activateSuccess: 'User with email {0} activated in the system.'
+        activateSuccess: 'User with email {0} activated in the system.',
+        deleteSuccess: 'User with id {0} has been deleted from the system'
     },
     error: {
         user: {
             multipleEmail: 'User exists in the system.',
             usernameUnavailable: 'Username is not available. Please use other username.',
             userNotFoundByEmail: 'Cannot find user registered to our system with email \'{0}\'',
-            updateActivation: 'Cannot activate user with email \'{0}\''
+            userNotFoundById: 'Cannot find user registered to our system with id {0}',
+            updateActivation: 'Cannot activate user with email \'{0}\'',
+            updateDelete: 'Cannot delete user with id {0}'
         } 
     }
 }
