568171ed67e72f4450c19927dcf9247b7c0f1243
Prajesh Raj Kayastha <94prajesh@gmail.com>
2020-03-31 15:13:09 +1030

    enable cors

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var favicon = require('static-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+const cors = require('cors');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
@@ -26,6 +27,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors());
 
 db.sync();
 
